e14264c52429c73e013a41b109e4f8a0ed920963
Ryszard Solecki <57400708+Carroway256@users.noreply.github.com>
2024-04-28 15:05:01 +0200

    Delete src/env.ts

--- src/env.ts
+++
@@ -1,7 +0,0 @@
-export const ACCESS_TOKEN_SECRET='e6b841a7f3485129778ed63e95a2299bcb5750bead42a951700d62169608a13bf5019edb9162b38fe2dffe9bea39a846310adc4d7977c336a9e5792063d704a3'
-// DB_URL = "mongodb+srv://Ash:Pikachu123@cluster0.uwxcgma.mongodb.net/?retryWrites=true&w=majority"
-// DD_URL = "mongodb+srv://${}:${}@cluster0.uwxcgma.mongodb.net/?retryWrites=true&w=majority"
-// MONGO_USERNAME = "Ash"
-// MONGO_PASSWORD = "Pikachu123"
-// eyJhbGciOiJIUzI1NiJ9.YWRtaW4.tyoh5JwfIUUFvA25oGz768utsPJeByk55RceBbZrHE8
-export const MONGO_URL =   "mongodb+srv://Ash:Pikachu123@cluster0.uwxcgma.mongodb.net/?retryWrites=true&w=majority"
